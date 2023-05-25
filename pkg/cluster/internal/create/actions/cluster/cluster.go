6ca15b2ee9b7e6ce2f363030e43422b4cef1a1b2
esierra-stratio <esierra@stratio.com>
2023-05-25 12:57:35 +0200

    fix: set fixed value of

--- pkg/cluster/internal/create/actions/cluster/cluster.go
+++ pkg/cluster/internal/create/actions/cluster/cluster.go
@@ -62,8 +62,6 @@ type DescriptorFile struct {
 	ExternalDomain string `yaml:"external_domain" validate:"omitempty,hostname"`
 
 	Keos struct {
-		// PR fixing exclude_if behaviour https://github.com/go-playground/validator/pull/93
-		Domain  string `yaml:"domain" validate:"omitempty,hostname"`
 		Flavour string `yaml:"flavour"`
 		Version string `yaml:"version"`
 	} `yaml:"keos"`
