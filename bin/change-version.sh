f9b0c480b0b570cc8930c3f16e98947b2b937321
Unai Arríen <unaittxu@gmail.com>
2025-03-19 16:00:44 +0100

    Automate cloud-provisioner version management within CICD flow (#716)

--- bin/change-version.sh
+++ bin/change-version.sh
@@ -10,5 +10,11 @@ else
 	VERSION=$1
 fi
 
+VERSION_GO_FILE="$BASEDIR/pkg/cmd/kind/version/version.go"
+CORE_VERSION=$(echo "$VERSION" | sed -E "s/-.*//")
+
 echo "Modifying cloud-provisioner version to: $1"
-echo $VERSION > VERSION
+echo $VERSION > $BASEDIR/VERSION
+
+sed -i "s/\(const versionCore = \"\)[^\"]*\"/\10.17.0-$CORE_VERSION\"/" "$VERSION_GO_FILE"
+sed -i "s/\(const versionPreRelease = \"\)[^\"]*\"/\1SNAPSHOT\"/" "$VERSION_GO_FILE"
